d85aae288060be17201e954e275ec0bcdb571d02
Viktoriya Jivkova <Jivkovaviktoria@gmail.com>
2023-01-31 15:15:49 +0200

    Implemented "GetAnimalsAsync" method

---
+++ AnimalAPI/Controllers/AnimalsController.cs
@@ -0,0 +1,31 @@
+﻿using AnimalAPI.Data;
+using AnimalAPI.Models;
+using AnimalAPI.Models.InputModels;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+
+namespace AnimalAPI.Controllers;
+
+[ApiController]
+[Route("api/[controller]")]
+public class AnimalsController : Controller
+{
+    private readonly AnimalsDbContext _context;
+
+    public AnimalsController(AnimalsDbContext context)
+    {
+        this._context = context;
+    }
+
+    [HttpGet]
+    public async Task<IActionResult> GetAnimalsAsync()
+    {
+        return Ok(await this._context.Animals.ToListAsync());
+    }
+}
+    
+    
+        
+    
+    
+    
